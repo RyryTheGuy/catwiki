0135f8b2885b6735b4d3f81d5358d16adb014ddf
RyryTheGuy <ryanroemer61@gmail.com>
2022-01-25 19:07:33 -0600

    Changed css naming to BEM convention

--- pages/index.js
+++ pages/index.js
@@ -51,29 +51,32 @@ export default function Home({ catNames, fourCats }) {
 
       {/* CatWiki Search */}
       <header className={styles.header}>
-        <div className={styles.catBg}>
-          <div style={{ width: '33%'}}>
+        <div className={styles['cat-background']}>
+          <div className={styles['cat-background__container']}>
             <Image src='/CatWikiLogo.svg' alt='Cat Wiki Logo' height={100} width={300} className={utilStyles.whiteLogo} />
-            <h3 style={{ marginBottom: '5rem', fontWeight: 'normal' }}>
+            <h3 className={styles['cat-background__title']}>
               Get to know more about your cat breed
             </h3>
             {/* Search bar */}
-            <SearchBar catNames={catNames} handleCatSelection={(catId) => console.log('Cat chosen!', catId)} />
-              {/* todo: add the dropdown with the search results */}
+            <SearchBar 
+              catNames={catNames} 
+              handleCatSelection={(catId) => console.log('Cat chosen!', catId)} 
+            />
           </div>
         </div>
 
-        <div className={styles.discover}>
-          <h4 className={utilStyles.underline} style={{ fontWeight: 'normal' }}>Most Searched Breeds</h4>
-          <div className={styles.discoverTitle}>
-            <h2 style={{ maxWidth: '30rem'}}>66+ Breeds For you to discover</h2>
-            <a style={{ alignSelf: 'flex-end'}}>SEE MORE &#8594;</a>
+        <div className={styles['discover-breeds']}>
+          <p className={utilStyles.underline}>Most Searched Breeds</p>
+          <div className={styles['discover-breeds__title-container']}>
+            <h2 className={styles['discover-breeds__title']}>66+ Breeds For you to discover</h2>
+            {/* todo: make this work */}
+            <a className={styles['discover-breeds__link']}>SEE MORE &#8594;</a>
           </div>
-          <div className={styles.catDisplay}>
+          <div className={styles['cat-display']}>
             {fourCats.map((cat) => (
               <div key={cat.id}>
                 {/* Cat Image */}
-                <div className={styles.catImage}>
+                <div className={styles['cat-display__image']}>
                   <Image src={cat.image?.url ?? '/kitten-silhouette-2993fc-lg+copy_ForgottenKitten_2.jpg'} alt={cat.name} layout='fill' objectFit='cover' />
                 </div>
                 {/* Cat Name */}
@@ -81,11 +84,47 @@ export default function Home({ catNames, fourCats }) {
               </div>
             ))}
           </div>
-          <div>
-
-          </div>
         </div>
       </header>
+
+      <section className={styles['cat-benefits']}>
+        <div className={styles['benefits-container']}>
+          <h2 className={styles['benefits-container__title']}>Why should you have a cat?</h2>
+          <p className={styles['benefits-container__text']}>
+            Having a cat around you can actually trigger the release of calming chemicals in your body which lower your stress and anxiety levels.
+          </p>
+          {/* todo: make this work */}
+          <a className={styles['benefits-container__link']}>READ MORE &#8594;</a>
+        </div>
+        <div className={styles['benefits-container--flex']} style={{ display: 'flex' }}>
+          <div className={styles['image-container']}>
+            <div className={styles['image2']}>
+              <Image 
+                src='/image 2.png'
+                alt='Cat Image'
+                layout='fill'
+                objectFit='cover'
+              />
+            </div>
+            <div className={styles['image1']}>
+              <Image 
+                src='/image 1.png'
+                alt='Cat Image'
+                layout='fill'
+                objectFit='cover'
+              />
+            </div>
+          </div>
+          <div className={styles['image3']}>
+            <Image 
+              src='/image 3.png'
+              alt='Cat Image'
+              layout='fill'
+              objectFit='cover'
+            />
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
